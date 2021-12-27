03cb685412ed3eafe88ed6e394230b1772451aaa
kapepa <kapepayx@gmail.com>
2021-12-27 23:43:57 +0200

    27.12

--- client/pages/room/[id].tsx
+++ client/pages/room/[id].tsx
@@ -8,15 +8,15 @@ import {IRoom} from "../../dto/room";
 
 interface IRoomPage {
   rooms: IRoom
-}
+};
 
 const Room: NextPage<IRoomPage> = ({room}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   console.log(room)
   return (
-    <BaseWrapper title={"Room"} description={"weclcome to room page"}>
+    <BaseWrapper title={room.title} description={`weclcome to room page ${room.title}`}>
       <div className={`flex flex-column ${style.room}`}>
         <div className={style.room__title}>
-          <h4 className={style.room__h4}>Name Room</h4>
+          <h4 className={style.room__h4}>{room.title}</h4>
         </div>
         <ListSpeakers {...room}/>
       </div>
