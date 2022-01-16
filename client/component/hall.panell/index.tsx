662f1dc70503e721c48667a0938f704acfedbc9a
kapepa <kapepayx@gmail.com>
2022-01-16 23:50:23 +0200

    16.01

--- client/component/hall.panell/index.tsx
+++ client/component/hall.panell/index.tsx
@@ -1,23 +1,28 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import style from "./hall.panell.module.scss";
 import Button from "../button";
+import PopupRoom from "../popup.room/indes";
 
 interface IHallPanell {
   title: string;
 }
 
 const HallPanell: FC<IHallPanell> = ({title}) => {
-  const ClickCreateRoom = () => {
+  const [popup, setPopup] = useState<boolean>(true);
 
-  }
+  const ClickCreateRoom = (e: React.MouseEvent<HTMLButtonElement>): void => setPopup(true);
+  const ClickClosePopupRoom = (): void => setPopup(false);
 
   return (
-    <div className={`flex justify-space-between align-center ${style.hall_panell}`}>
-      <h5 className={style.hall_panell__h5}>{title}</h5>
-      <div>
-        <Button name={"Start a room"} callback={ClickCreateRoom} color={'green'}/>
+    <>
+      {popup && <PopupRoom callback={ClickClosePopupRoom}/>}
+      <div className={`flex justify-space-between align-center ${style.hall_panell}`}>
+        <h5 className={style.hall_panell__h5}>{title}</h5>
+        <div>
+          <Button name={"Start a room"} callback={ClickCreateRoom} color={'green'}/>
+        </div>
       </div>
-    </div>
+    </>
   )
 };
 
