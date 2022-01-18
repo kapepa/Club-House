062a1858b27485f3f2878c5f967474ca2ca225e3
kapepa <kapepayx@gmail.com>
2022-01-18 23:43:08 +0200

    18.01

--- client/component/room.card/index.tsx
+++ client/component/room.card/index.tsx
@@ -7,7 +7,6 @@ import Avatar from "../avatar";
 
 const RoomCard: FC<IRoom> = (props) => {
   const {id,title, message, people, speaker} = props
-  console.log(props)
   const router = useRouter();
   return (
     <div
@@ -18,17 +17,17 @@ const RoomCard: FC<IRoom> = (props) => {
         <h4>{title}</h4>
         <div className="flex">
           <div className={`flex flex-column ${style.room_card__avatar}`}>
-            {/*{speaker.map((user: {avatar: string}, i: number) => {return (*/}
-            {/*  <div key={user.avatar + i} className={`${style.room_card__image}`}>*/}
-            {/*    <Avatar size={30} url={user.avatar}/>*/}
-            {/*  </div>)})}*/}
+            {speaker.map((user: {avatar: string}, i: number) => {return (
+              <div key={user.avatar + i} className={`${style.room_card__image}`}>
+                <Avatar size={30} url={user.avatar}/>
+              </div>)})}
           </div>
           <div className={`flex flex-column ${style.room_card__desc}`}>
             <div className={`flex flex-column ${style.room_card__name}`}>
-              {/*{speaker.map((user: {username: string}, i: number) => <span*/}
-              {/*  key={user.username + i}*/}
-              {/*  className={style.room_card__speaker}*/}
-              {/*>{user.username}</span>)}*/}
+              {speaker.map((user: {username: string}, i: number) => <span
+                key={user.username + i}
+                className={style.room_card__speaker}
+              >{user.username}</span>)}
             </div>
             <div className={`flex`}>
               <div className={`${style.room_card__icon} ${style.room_card__people}`}>{people}</div>
