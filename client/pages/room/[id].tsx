085f6ebee7c32d991d6ea71e68c0924dc8c86240
kapepa <kapepayx@gmail.com>
2022-01-20 23:55:13 +0200

    20.01

--- client/pages/room/[id].tsx
+++ client/pages/room/[id].tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import { useRouter } from 'next/router'
 import {InferGetServerSidePropsType, NextPage} from "next";
 import style from "./room.module.scss";
@@ -14,22 +14,33 @@ interface IRoomPage {
   rooms: IRoom,
 };
 
+interface IState {
+  owner: boolean
+}
+
 const Room: NextPage<IRoomPage> = ({room, user}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const route = useRouter();
+  const [state, setState] = useState({} as IState)
 
   const RemoveRoom = async (e: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
-    const ownUser = room.speaker.some((profile: IUser) => profile.id === user.id);
-    if(ownUser) await DeleteRoom(room.id).then( (res: boolean) => {
+    if(state.owner) await DeleteRoom(room.id).then( (res: boolean) => {
       if(res) route.push('/hall');
     })
   }
+
+  useRef(() => {
+    const ownCheck = room.speaker.some((profile: IUser) => profile.id === user.id);
+    setState({...state, owner: ownCheck})
+  }, []);
 
   return (
     <BaseWrapper title={room.title} description={`weclcome to room page ${room.title}`} userContext={user}>
       <div className={`flex flex-column ${style.room}`}>
         <div className={`${style.room__title} flex justify-space-between`}>
           <h4 className={style.room__h4}>{room.title}</h4>
-          <Button classes={style.room__delete} name={'Delete room'} callback={RemoveRoom} color={'red'}/>
+          <div>
+            <Button classes={style.room__delete} name={'Delete room'} callback={RemoveRoom} color={'red'}/>
+          </div>
         </div>
         <ListSpeakers {...room}/>
       </div>
